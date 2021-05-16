ad09d1473f090656ceba1dcb2db9d4481ba270bb
samuel hurtado <aamacanitan@gmail.com>
2021-05-16 13:25:58 -0400

    Api Google

--- js/java.js
+++ js/java.js
@@ -88,4 +88,14 @@ function validacion2(){
 
 }
 
-
+function iniciarMap(){
+    var coord = {lat:-33.4179935 ,lng: -70.6085841};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
