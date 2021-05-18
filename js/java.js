31e4397b463d15f64bcd30ba21822ca18509c4b8
samuel hurtado <aamacanitan@gmail.com>
2021-05-17 23:44:12 -0400

    Ultimas actualizaciones

--- js/java.js
+++ js/java.js
@@ -1,90 +1,165 @@
-window.onscroll = function(){
-
-    if(document.documentElement.scrollTop){
-        document.querySelector('.go-top-container')
-        .classList.add('show');
-    }else{
-        document.querySelector('.go-top-container')
-        .classList.remove('show');
+window.onscroll = function() {
+    console.log(document.documentElement.scrollTop);
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container').classList.add('show');
+    } else {
+        (document.documentElement.scrollTop > 100)
+        document.querySelector('.go-top-container').classList.remove('show');
     }
-    
 
 }
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
 
 function validacion(){
     var nombre, email, fono, fechan,contra, direccion;
     nombre= document.getElementById('nombre').value;
     email = document.getElementById('email').value; 
-    fono = document.getElementById('telefono').value;
+    fono = document.getElementById('fono').value;
     fechan = document.getElementById('fechan').value;
     contra = document.getElementById('contra').value;
     direccion =document.getElementById('direccion').value;
    
-    expresion= /\w+@\.+[a-z]/;
-
-    if(nombre === "" || email === "" || fono === "" || fechan === "" || contra === "" || direccion === ""){
-        alert("Todos los campos son obligatorio");
+    var emailR = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(nombre === ""){
+        alert("Ingrese su nombre");
         return false
     }
-    else if(nombre.length>70){
-        alert("El nombre es muy largo");
+    if(nombre.length<10){
+        alert("Minimo 10 caracteres");
         return false
     }
-    else if(email.length>40){
-        alert("El email es muy largo")
+    if( email === ""){
+        alert("Ingrese su email");
+        return false
     }
-    else if(fono.length>10){
-        alert("El telefono es muy largo")
+    if(!emailR.test(email)){
+        alert("El correo no es válido");
+        return false
     }
-    else if(!expresion.test(correo)){
-        alert("El correo no es válido")
+    
+    if(fono === ""){
+        alert("Ingrese su telefono");
+        return false
     }
-
-    else if(contra.length<6){
-        alert("No puede tener menos de 6 caracteres")
+    if(fono.length<9){
+        alert("Minimo 9 caracteres");
+        return false
     }
-    else if(direccion.length>40){
-        alert("Direccion muy larga")
+    if(fono.length>9){
+        alert("Max 9 caracteres");
+        return false
     }
-    else if(isNaN(fono)){
-        alert("El teléfono ingresado no es un número")
+    if(isNaN(fono)){
+        alert("El teléfono ingresado no es un número");
+        return false
     }
-    return false
+    if(fechan === "" ){
+        alert("Ingrese su fecha de nacimiento");
+        return false
+    }
+    if( contra === ""){
+        alert("Ingrese contraseña");
+        return false  
+    }
+    if(contra.length<6){
+        alert("Minimo 6 caracteres");
+        return false
+    }
+    if( direccion === ""){
+        alert("Ingrese su direccion");
+        return false
+    }
+    if( direccion.length<5){
+        alert("Minimo 5 caracteres");
+        return false
+    }
+ 
 
 }
 
 function validacion2(){
-    var nombre, email, fono,  direccion, mensaje;
-    nombre= document.getElementById('nombre').value;
+    var zapa,talla, email, fono,envio,  direccion, motivo ;
+    zapa= document.getElementById('zapatilla').value;
+    talla= document.getElementById('talla').value;
     email = document.getElementById('email').value; 
     fono = document.getElementById('telefono').value;
+    envio = document.getElementById('envio').value; 
     direccion =document.getElementById('direccion').value;
-    mensaje = document.getElementById('mensaje').value;
+    motivo = document.getElementById('motivo').value;
+    
+    
 
-    if(nom === "" || email === "" || fono === "" ||  direccion === "" || mensaje === ""){
-        alert("Todos los campos son obligatorio");
+    var emailR = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(zapa === ""){
+        alert("Ingrese modelo de zapatilla");
         return false
     }
-    else if(nombre.length>70){
-        alert("El nombre es muy largo");
+    if(zapa.length<5){
+        alert("Minimo 5 caracteres");
         return false
     }
-    else if(email.length>40){
-        alert("El email es muy largo")
+    if(talla === ""){
+        alert("Ingrese la talla de su zapatilla");
+        return false
     }
-    else if(!expresion.test(correo)){
-        alert("El correo no es válido")
-    }
-    else if(fono.length>10){
-        alert("El telefono es muy largo")
-    }
-    else if(isNaN(fono)){
-        alert("El teléfono ingresado no es un número")
-    }
-    else if(direccion.length>40){
-        alert("Direccion muy larga")
-    }
+    if(talla!="8" && talla!="8.5" && talla!="9" && talla!="9.5" && talla!="10" && talla!="10.5" && talla!="11" && talla!="11.5" && talla!="12"){
+        alert("Solo pueden ser 8 /8,5 /9 /9,5 /10 /10,5 /11 /11,5 /12");
+        return false
 
+    }
+    if( email === ""){
+        alert("Ingrese su email");
+        return false
+    }
+    if(!emailR.test(email)){
+        alert("El correo no es válido");
+        return false
+    }
+    
+    if(fono === ""){
+        alert("Ingrese su telefono");
+        return false
+    }
+    if(fono.length<9){
+        alert("Minimo 9 caracteres");
+        return false
+    }
+    if(fono.length>9){
+        alert("Max 9 caracteres");
+        return false
+    }
+    if(isNaN(fono)){
+        alert("El teléfono ingresado no es un número");
+        return false
+    }
+    if(envio === ""){
+        alert("Ingrese su codigo de envio")
+        return false
+    }
+    if(envio.length<10){
+        alert("Codigo de envio no valido minimo 10 caracteres");
+        return false
+    }
+    
+    if( direccion === ""){
+        alert("Ingrese su direccion");
+        return false
+    }
+    if( direccion.length<5){
+        alert("Minimo 5 caracteres");
+        return false
+    }
+    if(motivo === ""){
+        alert("Ingrese el motivo de su devolucion")
+        return false
+    }
+    
 
 }
 
--- js/script.js
+++ js/script.js
@@ -1,97 +1,17 @@
-window.onscroll = function(){
-
-    if(document.documentElement.scrollTop){
-        document.querySelector('.go-top-container')
-        .classList.add('show');
-    }else{
-        document.querySelector('.go-top-container')
-        .classList.remove('show');
+window.onscroll = function() {
+    console.log(document.documentElement.scrollTop);
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container').classList.add('show');
+    } else {
+        (document.documentElement.scrollTop > 100)
+        document.querySelector('.go-top-container').classList.remove('show');
     }
-    
 
 }
 
-function validacion(){
-    var nombre, email, fono, fechan,contra, direccion;
-    nombre= document.getElementById('nombre').value;
-    email = document.getElementById('email').value; 
-    fono = document.getElementById('telefono').value;
-    fechan = document.getElementById('fechan').value;
-    contra = document.getElementById('contra').value;
-    direccion =document.getElementById('direccion').value;
-   
-    expresion= /\w+@\.+[a-z]/;
-
-    if(nombre === "" || email === "" || fono === "" || fechan === "" || contra === "" || direccion === ""){
-        alert("Todos los campos son obligatorio");
-        return false
-    }
-    else if(nombre.length>70){
-        alert("El nombre es muy largo");
-        return false
-    }
-    else if(email.length>40){
-        alert("El email es muy largo")
-    }
-    else if(fono.length>10){
-        alert("El telefono es muy largo")
-    }
-    else if(!expresion.test(correo)){
-        alert("El correo no es válido")
-    }
-
-    else if(contra.length<6){
-        alert("No puede tener menos de 6 caracteres")
-    }
-    else if(direccion.length>40){
-        alert("Direccion muy larga")
-    }
-    else if(isNaN(fono)){
-        alert("El teléfono ingresado no es un número")
-    }
-    return false
-
-}
-
-function validacion2(){
-    var nombre, email, fono,  direccion, mensaje;
-    nombre= document.getElementById('nombre').value;
-    email = document.getElementById('email').value; 
-    fono = document.getElementById('telefono').value;
-    direccion =document.getElementById('direccion').value;
-    mensaje = document.getElementById('mensaje').value;
-
-    if(nom === "" || email === "" || fono === "" ||  direccion === "" || mensaje === ""){
-        alert("Todos los campos son obligatorio");
-        return false
-    }
-    else if(nombre.length>70){
-        alert("El nombre es muy largo");
-        return false
-    }
-    else if(email.length>40){
-        alert("El email es muy largo")
-    }
-    else if(!expresion.test(correo)){
-        alert("El correo no es válido")
-    }
-    else if(fono.length>10){
-        alert("El telefono es muy largo")
-    }
-    else if(isNaN(fono)){
-        alert("El teléfono ingresado no es un número")
-    }
-    else if(direccion.length>40){
-        alert("Direccion muy larga")
-    }
-
-
-}
-
-function iniciarMap(){
-    var coord= {lat:-33.477669 ,lng:-70.642364};
-    var map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 10,
-        center: coord
-    })
-}
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
